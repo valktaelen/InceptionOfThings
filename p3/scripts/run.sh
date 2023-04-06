dbaee011c2a24ec23680edd28ae6e8ae525cd3dc
valktaelen <alix.artiges@hotmail.fr>
2023-04-07 00:10:20 +0200

    end p3

--- p3/scripts/run.sh
+++ p3/scripts/run.sh
@@ -21,19 +21,26 @@ sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-
 sudo kubectl apply -f confs/config.yml
 
 
-while [ true ] ; do
-	i=$(sudo kubectl get pod -n argocd | grep "1/1" | wc -l)
+while true ; do
+	i=$(sudo kubectl get pod -n argocd 2>/dev/null | grep "1/1" | wc -l)
 	if [ $i = 7 ] ; then
 		echo "Ready!"
 		break
 	else
-		echo -n "Initializing cluster..."
+		echo -n "."
 		sleep 10
 	fi
 done
 
-sudo kubectl port-forward -n argocd svc/argocd-server 8080:443
-sudo kubectl port-forward -n dev svc/app-wil 8888:8888
+sudo kubectl port-forward -n argocd svc/argocd-server 8080:443 1>/dev/null &
+
+sudo bash << EOF & &>/dev/null
+while true ; do
+	sudo kubectl port-forward -n dev svc/wil-app 8888:8888 &>/dev/null
+	sleep 10
+done
+EOF
 
 echo 'Password: '
 sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath=”{.data.password}” | sed 's/^.//;s/.$//' | base64 -d
+echo
