74e0efb2cb954594e4374a4762f2c0693a8c010c
valktaelen <alix.artiges@hotmail.fr>
2023-04-12 02:11:04 +0200

    change ip

--- p1/scripts/provision-worker.sh
+++ p1/scripts/provision-worker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-JOIN_TOKEN=$(ssh -o StrictHostKeyChecking=no -i /home/vagrant/.ssh/id_rsa vagrant@192.168.42.110 "sudo cat /var/lib/rancher/k3s/server/node-token")
+JOIN_TOKEN=$(ssh -o StrictHostKeyChecking=no -i /home/vagrant/.ssh/id_rsa vagrant@192.168.56.110 "sudo cat /var/lib/rancher/k3s/server/node-token")
 
-export K3S_URL=https://192.168.42.110:6443
+export K3S_URL=https://192.168.56.110:6443
 export K3S_TOKEN=$JOIN_TOKEN
 export INSTALL_K3S_EXEC="--flannel-iface=eth1"
 
