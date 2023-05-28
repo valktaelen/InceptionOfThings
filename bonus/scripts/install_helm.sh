eadd1d7aba79b62a1d843b2fc9b8953c7459d66f
aartiges <99112556+valktaelen@users.noreply.github.com>
2023-05-29 00:09:42 +0200

    Delete bonus directory

--- bonus/scripts/install_helm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-install_helm() {
-    echo 'Install helm'
-	curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | sudo bash
-}
--- bonus/scripts/install_k3d.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-install_k3d() {
-    echo 'Install k3d'
-    wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
-}
